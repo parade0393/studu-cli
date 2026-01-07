ec217807f86c24d845df024a9c0e2ed1e4010a11
flb <46487578+parade0393@users.noreply.github.com>
2026-01-07 17:24:38 +0800

    fix: 修复commitlint的配置

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -21,6 +21,6 @@ export default {
     'type-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 95], //和config-conventional的prompt一致
   },
 }
